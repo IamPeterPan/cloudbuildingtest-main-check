3dc23af7a2247d7f4d6c17ed44019138cb4bb008
IamPeterPan <31070398+IamPeterPan@users.noreply.github.com>
2021-12-14 20:57:00 +0530

    Update main.go

--- main.go
+++ main.go
@@ -9,12 +9,25 @@ import (
 
 
 func main() {
+    log.Print("starting server...")
+// 	http.HandleFunc("/", handler)
+
+// 	// Determine port for HTTP service.
+// 	port := os.Getenv("PORT")
+// 	if port == "" {
+// 		port = "8080"
+// 		log.Printf("defaulting to port %s", port)
+// 	} 
 
     records := [][]string{
-        {"first_name", "last_name", "occupation"},
-        {"John", "Doe", "gardener"},
-        {"Lucy", "Smith", "teacher"},
-        {"Brian", "Bethamy", "programmer"},
+        {"date", "time", "currency"},
+        {"11/11/2021", "3:00 PM", "BTC 1"},
+        {"12/10/2021", "4:00 PM", "BTC 2"},
+        {"10/10/2021", "5:00 PM", "BTC 3"},
+        {"13/11/2021", "6:00 PM", "BTC 4"},
+        {"14/10/2021", "7:00 PM", "BTC 5"},
+        {"15/10/2021", "8:00 PM", "BTC 6"},
+        
     }
 
     f, err := os.Create("users.csv")
